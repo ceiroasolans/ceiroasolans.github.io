7a904ae42049bae72d9a659ca70dd8a50f8eb2db
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-12 12:12:29 -0700

    update

--- script.js
+++ script.js
@@ -259,16 +259,21 @@ function createEmotionGraph(videoId, onSubmit) {
 
     // Mapping of emotions to coordinates
     const emotions = {
-        "Angry": [30, 90],
-        "Fearful": [15, 80],
+        "Angry": [25, 90],
+        "Fearful": [15, 83],
+        "Horrified": [10, 73]
         "Disgusted": [10, 65],
         "Sad": [10, 30],
+        
         "Fatigued": [35, 10],
         "Calm": [60, 10],
         "Content": [75, 40],
-        "Happy": [85, 60],
+
+        "Joy": [90, 60],
+        "Adoration": [90, 55]
         "Elated": [85, 80],
-        "Excited": [60, 90]
+        "Amused": [60, 90],
+        "Excited": [65, 90]
     };
 
     // Add the emotions to the SVG
@@ -298,7 +303,7 @@ function createEmotionGraph(videoId, onSubmit) {
             }
     
     
-            const neutralLabel = createText2(190, 210, "Neutral");
+            const neutralLabel = createText2(175, 205, "Neutral");
             emotionGraph.appendChild(neutralLabel);
     
 
