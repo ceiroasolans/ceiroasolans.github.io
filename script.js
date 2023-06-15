3d316a9fb4d64b877611dae72824d0b8d015d136
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-15 14:50:50 -0700

    update

--- script.js
+++ script.js
@@ -266,14 +266,14 @@ function createEmotionGraph(videoId, onSubmit) {
     // Mapping of emotions to coordinates
     const emotions = {
         "Angry": [25, 90],
-        "Fearful": [15, 83],
+        "Fearful": [10, 83],
         "Horrified": [10, 73],
         "Disgusted": [10, 65],
         "Sad": [10, 30],
         //"Fatigued": [35, 10],
         "Calm": [60, 10],
         //"Content": [75, 40],
-        "Joyous": [90, 60],
+        "Joyous": [90, 65],
         "Adoration": [90, 55],
         "Excited": [85, 80],
         //"Elated": [85, 80],
