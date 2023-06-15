fb09b917f5e0410e48c9eef4c10a289a7c188d61
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-15 14:46:54 -0700

    update

--- script.js
+++ script.js
@@ -267,18 +267,25 @@ function createEmotionGraph(videoId, onSubmit) {
     const emotions = {
         "Angry": [25, 90],
         "Fearful": [15, 83],
-        //"Horrified": [10, 73],
+        "Horrified": [10, 73],
         "Disgusted": [10, 65],
         "Sad": [10, 30],
-        "Fatigued": [35, 10],
+        //"Fatigued": [35, 10],
         "Calm": [60, 10],
-        "Content": [75, 40],
-        "Happy": [90, 60],
-        //"Adoration": [90, 55],
-        "Elated": [85, 80],
-        //"Amused": [60, 90],
-        "Excited": [65, 90]
+        //"Content": [75, 40],
+        "Joyous": [90, 60],
+        "Adoration": [90, 55],
+        "Excited": [85, 80],
+        //"Elated": [85, 80],
+        "Amused": [60, 90],
+
+        //"Excited": [65, 90]
     };
+
+
+    //Adoration, Amusement, Excitement, Joy, Romance, Craving, Calmness, Awe, Interest, Anger, Sadness, Disgust, Fear, Horror
+
+
 
     // Add the emotions to the SVG
     for (let emotion in emotions) {
