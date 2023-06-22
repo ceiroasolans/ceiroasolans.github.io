5f36d77de851d5dbc70d04b66f9e66abd3bc8c14
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-22 12:32:29 -0700

    update

--- script.js
+++ script.js
@@ -316,6 +316,67 @@ function createRatingForm(videoId, onSubmit) {
     feedbackContainer.style.display = 'block';
 }
 
+//Watch again? 
+
+function createWatchAgainForm(onSubmit) {
+    feedbackContainer.innerHTML = '';
+
+    const questionObj = {
+        text: "Would you watch this video again?",
+        scale: ["No, never!", "Maybe", "Yes, anytime!"]
+    };
+
+    const responses = {};
+
+    const question = document.createElement("p");
+    question.textContent = questionObj.text;
+
+    const likertContainer = document.createElement("div");
+    likertContainer.classList.add("likert-container");
+
+    for(let i = 1; i <= 7; i++){
+        const likertBox = document.createElement("div");
+        likertBox.classList.add("likert-box");
+
+        const number = document.createElement("div");
+        number.textContent = i;
+        number.classList.add("likert-number");
+        likertBox.appendChild(number);
+
+        const label = document.createElement("div");
+        label.classList.add("likert-label");
+        likertBox.appendChild(label);
+
+        // Add labels on the edges and in the middle
+        if (i === 1) label.textContent += questionObj.scale[0];
+        else if (i === 4) label.textContent += questionObj.scale[1];
+        else if (i === 7) label.textContent += questionObj.scale[2];
+
+        likertBox.onclick = function() {
+            likertContainer.querySelectorAll(".likert-box").forEach(box => box.style.backgroundColor = "");
+            responses[questionObj.text] = i;
+            likertBox.style.backgroundColor = "#d8d8d8";  // Change color to indicate selection
+        };
+
+        likertContainer.appendChild(likertBox);
+    }
+
+    feedbackContainer.appendChild(question);
+    feedbackContainer.appendChild(likertContainer);
+
+    const submitButton = document.createElement("button");
+    submitButton.innerText = "Submit";
+    submitButton.onclick = () => {
+        if (responses[questionObj.text]) {
+            onSubmit(responses);
+        } else {
+            alert("Please answer the question.");
+        }
+    };
+
+    feedbackContainer.appendChild(submitButton);
+    feedbackContainer.style.display = "block";
+}
 
 
 
@@ -378,11 +439,7 @@ function generateVideoSequence(videosByType, order) {
   
 
 
-
-
-
-
-
+// EXPERIMENTAL
 
 function instructions() {
     let message = document.getElementById("message");
@@ -485,6 +542,8 @@ function experimentalSet() {
                                 createRatingForm(video.id, (ratings) => {
                                     console.log('Ratings submitted:', ratings);
                                     feedbackContainer.style.display = "none";
+                                    createWatchAgainForm(onSubmit  => {
+                                        feedbackContainer.style.display = "none";                                    
                                 showFixationCross(playNextVideo);
     
                                 participantChoices.push({
@@ -496,6 +555,7 @@ function experimentalSet() {
                                 });
                               });
                             });
+                        });
                         });
                     });
     
