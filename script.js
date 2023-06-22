42858d279f8825007295bce4d481872fad53d87e
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-22 09:42:30 -0700

    update

--- script.js
+++ script.js
@@ -175,377 +175,65 @@ function createFeedbackForm(videoId, onSubmit) {
     feedbackContainer.innerHTML = '';
 
     const questions = [
-        { text: "How do you feel?", scale: ["Quiet, still, inactive", " ", " ", "Neutral", " ", " ", "Activated, intense, aroused"] },
-        { text: " ", scale: ["Negative, dissatisfied, unhappy ", " ", " ", "Neutral", " ", " ","Positive, satisfied, pleased"] }
-    ];
-
-    const responses = {};
-
-    questions.forEach(questionObj => {
-        const question = document.createElement("p");
-        question.textContent = questionObj.text;
-
-        const responseContainer = document.createElement("div");
-        responseContainer.style.display = "flex";
-        responseContainer.style.justifyContent = "space-between";
-        responseContainer.style.marginBottom = "20px";  // Add some vertical separation between questions
-
-        questionObj.scale.forEach((label, index) => {
-            const button = document.createElement("button");
-            button.textContent = label;
-            button.style.flexGrow = "1";
-            button.style.backgroundColor = "#f0f0f0";  // A light shade of grey
-            button.onclick = function() {
-                responses[questionObj.text] = index;
-                button.style.backgroundColor = "lightblue";  // Change color to indicate selection
-            };
-            responseContainer.appendChild(button);
-        });
-
-        feedbackContainer.appendChild(question);
-        feedbackContainer.appendChild(responseContainer);
-    });
-
-    const submitButton = document.createElement("button");
-    submitButton.innerText = "Submit";
-    submitButton.disabled = false;
-    submitButton.onclick = () => {
-        if (Object.keys(responses).length === questions.length) {
-            // Only submit if all questions have been answered
-            onSubmit(responses);
-        } else {
-            alert("Please answer all questions.");
-        }
-    };
-
-    feedbackContainer.appendChild(submitButton);
-    feedbackContainer.style.display = "block";
-}
-
-
-
-//  Response variable 2: Emotion circumplex
-function createEmotionGraph(videoId, onSubmit) {
-    const emotionGraphContainer = document.getElementById('emotionGraphContainer');
-    const emotionSubmit = document.getElementById('emotionSubmit');
-    const emotionGraph = document.getElementById('emotionGraph');
-
-    emotionSubmit.disabled = true; // Disable the submit button initially
-
-    // Clear any existing dots from the graph
-    const existingDots = emotionGraph.getElementsByClassName('emotion-dot');
-    while (existingDots[0]) {
-        existingDots[0].parentNode.removeChild(existingDots[0]);
-    }
-
-    // Create the x-axis line and add it to the SVG
-    const xAxisLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    xAxisLine.setAttribute("x1", 30);
-    xAxisLine.setAttribute("y1", 200);
-    xAxisLine.setAttribute("x2", 370);
-    xAxisLine.setAttribute("y2", 200);
-    xAxisLine.setAttribute("stroke", "black");
-    xAxisLine.setAttribute("stroke-width", 1);
-    emotionGraph.appendChild(xAxisLine);
-
-    // Create the y-axis line and add it to the SVG
-    const yAxisLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    yAxisLine.setAttribute("x1", 200);
-    yAxisLine.setAttribute("y1", 30);
-    yAxisLine.setAttribute("x2", 200);
-    yAxisLine.setAttribute("y2", 370);
-    yAxisLine.setAttribute("stroke", "black");
-    yAxisLine.setAttribute("stroke-width", 1);
-    emotionGraph.appendChild(yAxisLine);
-
-
-
-
-    // Create a function for creating text elements
-    function createText(x, y, text) {
-        const textElement = document.createElementNS("http://www.w3.org/2000/svg", "text");
-        textElement.setAttribute("x", x);
-        textElement.setAttribute("y", y);
-        textElement.textContent = text;
-        textElement.style.fontSize = "12px";
-        textElement.style.fontStyle = "italic";
-        textElement.setAttribute("fill", "#808080");
-        return textElement;
-    }
-
-    // Function to create line
-    function createLine(x1, y1, x2, y2) {
-        const lineElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        lineElement.setAttribute("x1", x1);
-        lineElement.setAttribute("y1", y1);
-        lineElement.setAttribute("x2", x2);
-        lineElement.setAttribute("y2", y2);
-        lineElement.setAttribute("stroke", "#D4D4D4"); // Light Grey Color
-        lineElement.setAttribute("stroke-width", 1);
-        return lineElement;
-    }
-
-
-
-
-    // Mapping of emotions to coordinates
-    const emotions = {
-        "Anger": [25, 90],
-        "Fear": [10, 83],
-       // "Horrified": [10, 73], //don't include?? 
-        "Disgust": [10, 62],
-        "Sadness": [10, 30],
-        //"Fatigued": [35, 10],
-        "Calmness": [63, 10],
-        //"Content": [75, 40],
-        "Joy": [90, 65],
-        "Affection": [85, 55], // Love and affection? or just Affection? 
-        "Awe": [70, 30],
-        "Excitement": [85, 80],
-        //"Elated": [85, 80],
-        "Amusement": [60, 90]
-    };
-
-
-    //Adoration, Amusement, Excitement, Joy, Romance, Craving, Calmness, Awe, Interest, Anger, Sadness, Disgust, Fear, Horror
-
-
-
-    // Add the emotions to the SVG
-    for (let emotion in emotions) {
-        const [xPercent, yPercent] = emotions[emotion];
-        const x = 4 * xPercent;
-        const y = 400 - (4 * yPercent);
-
-        // Create and add the line to the SVG before the text
-        const lineElement = createLine(x, y, 200, 200);
-        emotionGraph.appendChild(lineElement);
-
-        const textElement = createText(x, y, emotion);
-        emotionGraph.appendChild(textElement);
-    }
-
-            // Add "neutral" label to the midpoint (200, 200)
-            function createText2(x, y, text) {
-                const textElement = document.createElementNS("http://www.w3.org/2000/svg", "text");
-                textElement.setAttribute("x", x);
-                textElement.setAttribute("y", y);
-                textElement.textContent = text;
-                textElement.style.fontSize = "20px";
-                textElement.style.fontWeight = "bold";
-                textElement.setAttribute("fill", "#000000");
-                return textElement;
-            }
-    
-    
-            const neutralLabel = createText2(173, 205, "Neutral");
-            emotionGraph.appendChild(neutralLabel);
-    
-
-    let dot; // Declare the dot variable
-
-    const createDot = (e) => {
-        // Create the dot and add it to the SVG
-        dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        dot.setAttribute("cx", e.offsetX);
-        dot.setAttribute("cy", e.offsetY);
-        dot.setAttribute("r", 10);
-        dot.setAttribute("fill", "red");
-        dot.setAttribute("class", "emotion-dot");
-        emotionGraph.appendChild(dot);
-
-        // Remove the click event listener after the first click
-        emotionGraph.removeEventListener('click', createDot);
-        emotionSubmit.disabled = false; // Enable the submit button as the dot has been created
-    }
-
-    emotionGraph.addEventListener('click', createDot);
-
-    // Dragging state
-    let dragging = false;
-    let dotMoved = false;
-
-    const startDragging = (e) => {
-        // Ensure the drag only begins if the mouse is over the dot
-        if (e.target === dot) {
-            dragging = true;
-        }
-    };
-
-    const stopDragging = (e) => {
-        dragging = false;
-    };
-
-    const dragDot = (e) => {
-        if (dragging && dot) {
-            dotMoved = true;
-            let x = e.offsetX;
-            let y = e.offsetY;
-    
-            // Boundaries for SVG (400 x 400)
-            if (x < 30) x = 30;
-            if (x > 370) x = 370;
-            if (y < 30) y = 30;
-            if (y > 370) y = 370;
-    
-            dot.setAttribute("cx", x);
-            dot.setAttribute("cy", y);
-        }
-    };
-    
-
-    emotionGraph.addEventListener('mousedown', startDragging);
-    emotionGraph.addEventListener('mousemove', dragDot);
-    emotionGraph.addEventListener('mouseup', stopDragging);
-    emotionGraph.addEventListener('mouseleave', stopDragging);
-
-    // Handle submit button click
-    emotionSubmit.onclick = () => {
-        emotionGraphContainer.style.display = "none";
-        const valence = dot.getAttribute("cx");
-        const arousal = 400 - dot.getAttribute("cy");
-
-        onSubmit(valence, arousal);
-    };
-
-    emotionGraphContainer.style.display = "block";
-} 
-
-
-// Response variable: List of emotions (erase "2" in name and mute previous to work)
-// function createEmotionGraph2(videoId, onSubmit) {
-//     const emotionGraphContainer = document.getElementById('emotionGraphContainer');
-//     emotionGraphContainer.style.display = "flex";  // Change layout to Flexbox
-//     emotionGraphContainer.style.flexDirection = "row";
-//     emotionGraphContainer.style.justifyContent = "space-around";
-    
-
-//     const emotionSubmit = document.createElement("button");
-//     emotionSubmit.id = 'emotionSubmit';
-//     emotionSubmit.textContent = 'Submit';
-//     emotionSubmit.disabled = true; // Disable the submit button initially
-
-//     // Mapping of emotions to valence categories
-//     const emotions = {
-//         "Positive": ["Amusement", "Excitement", "Joy", "Heartwarming", "Love"],
-//         "Other": ["Craving", "Peacefulness", "Interest"],
-//         "Negative": ["Anger", "Sadness", "Disgust", "Fear"]
-//     };
-
-//     // Function to create an emotion item in the list
-//     function createEmotionItem(emotion) {
-//         const emotionItem = document.createElement("li");
-//         emotionItem.textContent = emotion;
-//         emotionItem.style.cursor = "pointer"; // Change cursor to pointer when hovering over the item
-
-//         // Add a click event listener to the item
-//         emotionItem.addEventListener("click", function() {
-//             if (emotionItem.classList.contains("selected")) {
-//                 emotionItem.classList.remove("selected"); // Deselect the item if it was already selected
-//             } else {
-//                 emotionItem.classList.add("selected"); // Select the item if it wasn't selected
-//             }
-
-//             // Check if any emotions are selected and enable/disable the submit button accordingly
-//             const selectedEmotions = document.getElementsByClassName('selected');
-//             emotionSubmit.disabled = selectedEmotions.length === 0;
-//         });
-
-//         return emotionItem;
-//     }
-
-//     // Clear any existing emotions from the container
-//     while (emotionGraphContainer.firstChild) {
-//         emotionGraphContainer.firstChild.remove();
-//     }
-
-//     // Create the list of emotions
-//     for (let valence in emotions) {
-//         const valenceContainer = document.createElement("div");  // Container for each valence category
-//         valenceContainer.style.flex = "1";  // Distribute space equally between the categories
-//         valenceContainer.style.margin = "10px";  // Add some margin around each category
-//         valenceContainer.style.display = "flex";  // Use Flexbox for the layout
-//         valenceContainer.style.flexDirection = "column";  // Stack the items vertically
-//         valenceContainer.style.alignItems = "center";  // Center the items
-
-//         const valenceHeader = document.createElement("h3");
-//         valenceHeader.textContent = valence;
-//         valenceContainer.appendChild(valenceHeader);
-
-//         const emotionList = document.createElement("ul");
-//         for (let emotion of emotions[valence]) {
-//             const emotionItem = createEmotionItem(emotion);
-//             emotionList.appendChild(emotionItem);
-//         }
-//         valenceContainer.appendChild(emotionList);
-//         emotionGraphContainer.appendChild(valenceContainer);
-//     }
-
-//     emotionGraphContainer.appendChild(emotionSubmit); // Add submit button to container
-
-//     // Handle submit button click
-//     emotionSubmit.onclick = () => {
-//         const selectedEmotions = document.getElementsByClassName('selected');
-//         let emotionsArray = [];
-//         for (let i = 0; i < selectedEmotions.length; i++) {
-//             emotionsArray.push(selectedEmotions[i].textContent);
-//         }
-//         emotionGraphContainer.style.display = "none";
-//         onSubmit(emotionsArray);
-//     };
-
-//     emotionGraphContainer.style.display = "block";
-// }
-
-// Response variable 3: Strategies
-function createFeedbackForm(videoId, onSubmit) {
-    feedbackContainer.innerHTML = '';
-
-    const questions = [
-        { text: "How do you feel?", scale: ["Quiet, still, inactive", " ", " ", "Neutral", " ", " ", "Activated, intense, aroused"] },
-        { text: " ", scale: ["Negative, dissatisfied, unhappy ", " ", " ", "Neutral", " ", " ","Positive, satisfied, pleased"] }
+        { text: "How do you feel?", scale: ["Quiet, still, inactive", "Neutral", "Activated, intense, aroused"] },
+        { text: "", scale: ["Negative, dissatisfied, unhappy", "Neutral", "Positive, satisfied, pleased"] }
     ];
 
     const responses = {};
     let buttons = [];
 
-    questions.forEach(questionObj => {
+    questions.forEach((questionObj, questionIndex) => {
         const question = document.createElement("p");
         question.textContent = questionObj.text;
+
+        const scaleContainer = document.createElement("div");
+        scaleContainer.style.display = "flex";
+        scaleContainer.style.justifyContent = "space-between";
+        scaleContainer.style.marginBottom = "20px";  // Add some vertical separation between questions
+
+        questionObj.scale.forEach((label, index) => {
+            const labelElement = document.createElement("div");
+            labelElement.textContent = label;
+            scaleContainer.appendChild(labelElement);
+        });
+
+        feedbackContainer.appendChild(question);
+        feedbackContainer.appendChild(scaleContainer);
 
         const responseContainer = document.createElement("div");
         responseContainer.style.display = "flex";
         responseContainer.style.justifyContent = "space-between";
-        responseContainer.style.marginBottom = "20px";
 
         let localButtons = [];
-        questionObj.scale.forEach((label, index) => {
+        for (let i = 1; i <= 7; i++) {
             const button = document.createElement("button");
-            button.textContent = label;
+            button.textContent = i;
             button.style.flexGrow = "1";
             button.style.backgroundColor = "#f0f0f0";
+            button.style.borderRadius = "50%";  // Make the buttons circular
+            button.style.width = "30px";
+            button.style.height = "30px";
             button.style.color = "black";  // Make text color always black
             button.onclick = function() {
-                if (responses[questionObj.text] === index) {
+                if (responses[questionIndex] === i) {
                     // Unclick action
-                    delete responses[questionObj.text];
+                    delete responses[questionIndex];
                     button.style.backgroundColor = "#f0f0f0";
                 } else {
                     // Deselect all other buttons in the same question
-                    localButtons.forEach((b, i) => {
-                        b.style.backgroundColor = i === index ? "#d3d3d3" : "#f0f0f0";
+                    localButtons.forEach((b, j) => {
+                        b.style.backgroundColor = j === i - 1 ? "#d3d3d3" : "#f0f0f0";
                     });
                     // Select this button
-                    responses[questionObj.text] = index;
+                    responses[questionIndex] = i;
                     button.style.backgroundColor = "#d3d3d3";
                 }
             };
             responseContainer.appendChild(button);
             localButtons.push(button);
-        });
+        }
 
         buttons = buttons.concat(localButtons);
-        feedbackContainer.appendChild(question);
         feedbackContainer.appendChild(responseContainer);
     });
 
