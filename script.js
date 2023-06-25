9d75fb0fca5b68abdc12c3fe34286bfe0688b897
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-24 20:41:39 -0700

    update

--- script.js
+++ script.js
@@ -725,7 +725,7 @@ function instructions3() {
 
     resultTableContainer.innerHTML = tableHtml;
 
-    showMessage("Congratulations! You have completed this study :)  )\n\n Here is your reported valence and arousal, averaged across emotions, compared to others.");
+    showMessage("Congratulations! You have completed this study :)  )\n\n\n Here is some feedback!");
     clearButtons();
     generateAndUploadCSV(participantChoices);
 }
