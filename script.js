ec8e3a53b8cbe6a611c9256958bd0a96e69d3cb6
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-27 16:23:09 -0700

    update

--- script.js
+++ script.js
@@ -420,7 +420,7 @@ function createRatingForm(videoId, onSubmit) {
         "Fear": ["angry", "sad", "disgusted", "afraid"]
     };
 
-    const video = videos.find(v => v.id === videoId); //change to v.id
+    const video = videos.find(v => v.id === videoId);
 
     if (!video) {
         console.error(`Video with id ${videoId} not found.`);
@@ -444,12 +444,7 @@ function createRatingForm(videoId, onSubmit) {
     feedbackContainer.appendChild(header);
 
     ratings.forEach((rating, index) => {
-        let likertContainer;
-        if (rating === ratings[2]) {
-            likertContainer = createLikertContainer(0, 6, `not ${rating} at all`, `very ${rating}`, rating, `somewhat ${rating}`);
-        } else {
-            likertContainer = createLikertContainer(0, 6, `not ${rating} at all`, `very ${rating}`, rating);
-        }
+        let likertContainer = createLikertContainer(0, 6, `not ${rating} at all`, `very ${rating}`, `somewhat ${rating}`, rating);
         likertContainer.id = `likert-${index + 1}`;
         likertContainer.style.marginBottom = '20px'; // Add spacing between the ratings
         feedbackContainer.appendChild(likertContainer);
@@ -473,7 +468,6 @@ function createRatingForm(videoId, onSubmit) {
                 EmoScore: parseInt(selectedBox.textContent),
                 vID: video.src
             });
-                
         }
 
         feedbackContainer.innerHTML = ''; // Clear the feedback container after successful submission
