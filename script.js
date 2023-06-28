94d4c9a10a49dc4449ec5893b99c306cf563ecb6
ceiroasolans <conrad.eiroa@gmail.com>
2023-06-27 21:08:14 -0700

    update

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const allVideos = [
     // {id: 92, src: "1832.mp4", type: "Fear" },
     // {id: 93, src: "1964.mp4", type: "Fear" },
     // {id: 94, src: "2091.mp4", type: "Fear" },
-    // {id: 29, src: "0723.mp4", type: "Interest" },
+    {id: 29, src: "0723.mp4", type: "Interest" },
     // {id: 30, src: "0735.mp4", type: "Interest" },
     // {id: 31, src: "1066.mp4", type: "Interest" },
     // {id: 32, src: "1068.mp4", type: "Interest" },
