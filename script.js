acaf7213272ec84f1ca272eed0a7d2cfce926aff
ceiroasolans <conrad.eiroa@gmail.com>
2023-08-12 16:25:54 +0200

    update

--- script.js
+++ script.js
@@ -722,6 +722,75 @@ function createRatingForm(videoId, onSubmit) {
 
 // Function to shuffle an array
 
+// function createWatchAgainForm(onSubmit) {
+//     feedbackContainer.innerHTML = '';
+
+//     const questionObj = {
+//         text: "Would you watch this video again?",
+//         scale: ["No, never!", "Maybe", "Yes, anytime!"]
+//     };
+
+//     const WatchAgainResponse = {};
+
+//     const question = document.createElement("p");
+//     question.style.fontWeight = 'bold'; 
+//     question.style.textAlign = 'center';
+//     question.textContent = questionObj.text;
+
+//     const likertContainer = document.createElement("div");
+//     likertContainer.classList.add("likert-container");
+
+//     for (let i = 0; i <= 6; i++) {
+//         const likertBox = document.createElement("div");
+//         likertBox.classList.add("likert-box");
+
+//         const number = document.createElement("div");
+//         number.textContent = i === 0 ? "0" : i.toString(); 
+//         number.classList.add("likert-number");
+//         likertBox.appendChild(number);
+
+//         const label = document.createElement("div");
+//         label.classList.add("likert-label");
+//         likertBox.appendChild(label);
+
+//         if (i === 0) label.textContent += questionObj.scale[0];
+//         else if (i === 3) label.textContent += questionObj.scale[1];
+//         else if (i === 6) label.textContent += questionObj.scale[2];
+
+//         // IIFE to correctly capture the value of i
+//         (function(currentIndex) {
+//             likertBox.onclick = function () {
+//                 console.log("Clicked value:", currentIndex); // Check the value when clicked
+//                 likertContainer.querySelectorAll(".likert-box").forEach(box => box.style.backgroundColor = "");
+//                 WatchAgainResponse[questionObj.text] = currentIndex;
+//                 likertBox.style.backgroundColor = "#d8d8d8";
+//                 console.log("Updated WatchAgain:", WatchAgainResponse); // Check the updated object
+//             };
+//         })(i);
+        
+
+//         likertContainer.appendChild(likertBox);
+//     }
+
+//     feedbackContainer.appendChild(question);
+//     feedbackContainer.appendChild(likertContainer);
+
+//     const submitButton = document.createElement("button");
+//     submitButton.innerText = "Submit";
+//     submitButton.onclick = () => {
+//         if (typeof WatchAgainResponse[questionObj.text] !== 'undefined') {
+//             onSubmit(WatchAgainResponse);
+//         } else {
+//             alert("Please answer the question.");
+//         }
+//     };
+
+//     feedbackContainer.appendChild(submitButton);
+//     feedbackContainer.style.display = "block";
+
+//     console.log("WatchAgain:", WatchAgainResponse);
+// }
+
 function createWatchAgainForm(onSubmit) {
     feedbackContainer.innerHTML = '';
 
@@ -779,6 +848,7 @@ function createWatchAgainForm(onSubmit) {
     submitButton.innerText = "Submit";
     submitButton.onclick = () => {
         if (typeof WatchAgainResponse[questionObj.text] !== 'undefined') {
+            WatchAgainResponse[questionObj.text] = WatchAgainResponse[questionObj.text].toString();
             onSubmit(WatchAgainResponse);
         } else {
             alert("Please answer the question.");
@@ -1216,8 +1286,8 @@ function generateAndUploadCSV(participantChoices) {
         row.videoType || "",
         row.EmoRated || "",
         row.EmoScore || "",
-        //row.watchAgain || "",
-        (typeof row.watchAgain !== 'undefined' ? row.watchAgain : ""),
+        row.watchAgain || "",
+        //(typeof row.watchAgain !== 'undefined' ? row.watchAgain : ""), // this works
         row.SID || "",
         row.uniqueKey || ""
       ];
