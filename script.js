42e54f4fd7b2fa8ab61e87df7895c456fd4b6b73
ceiroasolans <conrad.eiroa@gmail.com>
2023-08-16 06:42:21 -0400

    update

--- script.js
+++ script.js
@@ -731,7 +731,7 @@ let baselineEmoResponses = {};
 
 function baselineEmo() {
     // Top-anchor
-    document.body.classList.add('instructions-body-align');
+    //document.body.classList.add('instructions-body-align');
     document.body.style.alignItems = 'flex-start';
     window.scrollTo(0, 0);
 
@@ -745,7 +745,7 @@ function baselineEmo() {
     const header = document.createElement("p");
     header.style.fontWeight = 'bold';
     header.style.textAlign = 'center';
-    header.style.padding = '60px 0';
+    header.style.padding = '20px 0';
     header.textContent = "Please rate the extent to which you feel:";
     feedbackContainer.appendChild(header);
 
@@ -811,7 +811,7 @@ function baselineEmo() {
                 return acc;
             }, {});
             feedbackContainer.style.display = "none";
-            document.body.classList.remove('instructions-body-align'); 
+            //document.body.classList.remove('instructions-body-align'); 
             document.body.style.alignItems = '';
             instructions();
         } else {
