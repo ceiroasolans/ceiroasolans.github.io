abc32ddc98e410d8a8127447b92c67961cd62045
ceiroasolans <conrad.eiroa@gmail.com>
2023-08-16 07:36:35 -0400

    update

--- script.js
+++ script.js
@@ -695,9 +695,13 @@ nextButton.onclick = function() {
 
     let demoContainer = document.getElementById('demographicsContainer');
     demoContainer.parentNode.removeChild(demoContainer);  // Remove the demographics container from the DOM
+    if (!demoContainer) {
+        console.log("The element has been removed.");
+    } else {
+        console.log("The element is still present in the DOM.");
+    }
     document.body.classList.remove('instructions-body-align');
 
-    
     baselineEmo();  // Then display the baseline survey
 };
     
